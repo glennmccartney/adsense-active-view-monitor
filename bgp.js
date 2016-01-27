9dedb6f4931300f2bae036788e8e4fc39258aedf
gmc <glennmccartney@Glenn-MBP.home>
2016-01-27 19:44:40 +0000

    V1.0 First Commit

---
+++ bgp.js
@@ -0,0 +1,64 @@
+chrome.webRequest.onBeforeRequest.addListener(function(details) 
+{			
+	if((details.url.indexOf("activeview?") > -1)&&(details.type=="image"))
+  	{
+	  	var CurrentURL;
+	  	chrome.tabs.query({
+			active: true,
+			lastFocusedWindow: true
+		}, function(tabs) 
+			{
+				var slotID 
+			
+				if (details.url.indexOf("slotname%3D") > -1)
+				{
+					slotID = details.url.substr(details.url.indexOf("slotname%3D") + 11,10); 
+				}
+				else
+				{
+					slotID="N/A";
+				}	
+				var tab = tabs[0];
+				
+				if (tab)
+				{
+					CurrentURL=tab.url;
+					var opt = {
+					  type: "basic",
+					  title: "Active View Has Triggered",
+					  message: "Active View Has Triggered for Slot ID : " + slotID + " (" + CurrentURL+ ")",
+					  iconUrl: "icon.png"
+					}	
+					chrome.notifications.create(String(getRandomInt(0,9999)), opt, function(id) 
+					{
+					   if(chrome.runtime.lastError) {
+						 console.error(chrome.runtime.lastError.message);
+					   }
+					});
+				}
+				else
+				{
+					var opt = {
+					  type: "basic",
+					  title: "Active View Has Triggered",
+					  message: "Active View Has Triggered for Slot ID : " + slotID,
+					  iconUrl: "icon.png"
+					}
+					chrome.notifications.create(String(getRandomInt(0,9999)), opt, function(id) 
+					{
+					   if(chrome.runtime.lastError) 
+					   {
+						 console.error(chrome.runtime.lastError.message);
+					   }
+					});
+				}
+			});
+		}
+		return {cancel: false};
+	},{urls: ["<all_urls>"]
+});
+						
+function getRandomInt(min, max) 
+{
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
